eb69ab90845fe03c11ceab307002e01117618f7f
BonnorakSUN <sunby008@mymail.unisa.edu.au>
2025-03-24 13:29:51 +1030

    Commit for Section two

--- src/marking/Marker.java
+++ src/marking/Marker.java
@@ -34,7 +34,7 @@ public class Marker {
 		// Section TWO
 		// uncomment this section once you get to it
 		
-		/*
+		
 		Field testField = new Field(5, 5);
 		System.out.println(testField.get(3, 3)); //should print "."
 		System.out.println(testField); //should print the field
@@ -47,7 +47,11 @@ public class Marker {
 		testField.till(0, 0);
 		System.out.println(a.equals(testField.get(0, 0))); //should print false
 		System.out.println(testField.get(0, 0).equals(new Soil())); //should print true
-		*/
+		Apples apple = new Apples();
+		testField.plant(0, 0, apple);
+		testField.tick();
+		System.out.println(testField.getSummary());
+		
 
 		// Section THREE
 		// uncomment this section once you get to it
